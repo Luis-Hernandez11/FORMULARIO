79125709b53f5bb1dcc2c16ab3be7d9b1c7bea38
hluis4511@gmail.com <hluis4511@gmail.com>
2020-06-19 00:28:35 -0500

    calculador de edad, porcentaje de mujeres y hombres, año bisiesto

--- js/validacion.js
+++ js/validacion.js
@@ -2,18 +2,20 @@
 
 function validar(formulario) {
 
-    if (formulario.nombre.value.length < 3) {
-        alert("Escriba por lo menos 3 caracteres en el campo de nombre");
+    if (control("nombre").value.length >= 20) {
+        alert("Escriba por lo menos 20 caracteres en el campo de nombre");
         //focus nos auida a focalizar donde eta el error del llenado
         //realiza un posicioamiento al elemento del formulario
-        formulario.nombre.focus();
+        //formulario.nombre.focus();
+        contenido("nombre").value.focus();
         return false;
     }
 
     var checkOk = "QWERTYUIOPASDFGHJKLÑZXCVBNM" + "qwertyuiopasdfghjklñzxcvbnm"
-    var checkString = formulario.nombre.value:
+        //var checkString = formulario.nombre.value;
+    var checkString = contenindo("nombre").value;
 
-        alert(checkString);
+    alert(checkString);
 
     var allValid = true;
 
@@ -21,7 +23,7 @@ function validar(formulario) {
         //chekAt nos ayuda a separar una cadena de caracteres
         //por ejemplo ana como cadena en char sera A N A
         var ch = checkString.charAt(i);
-        for (int j = 0; j < checkOk.length; j++)
+        for (var j = 0; j < checkOk.length; j++)
             if (ch == checkOk.charAt(i))
                 break;
         if (j == checkOk.length) {
@@ -48,9 +50,9 @@ function validar(formulario) {
     }
 
     var checkOk = "123456789";
-    var checkString = formulario.edad.value:
+    var checkString = formulario.edad.value;
 
-        alert(checkString);
+    alert(checkString);
 
     var allValid = true;
     //debo saber si edad es all
@@ -72,18 +74,150 @@ function validar(formulario) {
 
     var txt = formulario.correo.value;
     //expresion regular
-    var b = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/
-        //ejemplo@ejemplo
-        //ejemplo@ejemplo.ejemplo
-        //todo lo que esta adentro de los corchetes es la expresion
-        //existen cadenas para creacion de curp
-        //para una llave compartida de sssdddpdf
+    // var b = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/
+    var b = /\w+@\w+\.+[a-z]/;
+    //ejemplo@ejemplo
+    //ejemplo@ejemplo.ejemplo
+    //todo lo que esta adentro de los corchetes es la expresion
+    //existen cadenas para creacion de curp
+    //para una llave compartida de sssdddpdf
 
     alert("Email " + (b.test(txt) ? "" : "no") + "valido")
         //si b.test(txt) "" es verdadero : sino el "no" es el falso
 
+    //FORMULARIO DE FECHA
+
+    //FORMULARIO DIA
+    if (formulario.d.value.length < 2 || formulario.d.value > 31) {
+        alert("Escriba por lo menos 2 digitos");
+        //focus nos auida a focalizar donde eta el error del llenado
+        //realiza un posicioamiento al elemento del formulario
+        formulario.d.focus();
+        return false;
+    }
+
+    var checkOk = "123456789"
+    var checkString = formulario.d.value;
+
+    alert(checkString);
+
+    var allValid = true;
+
+    if (!allValid) {
+        alert("Escriba solo numeros en el campo numerico");
+        formulario.d.focus();
+        return (false);
+    }
+
+    if (checkString);
+
+    //FORMULARIO MES
+    if (formulario.m.value.length < 2 || formulario.m.value > 13) {
+        alert("Escriba por lo menos 2 digitos para el mes y no mayor a 12");
+        //focus nos auida a focalizar donde eta el error del llenado
+        //realiza un posicioamiento al elemento del formulario
+        formulario.m.focus();
+        return false;
+    }
+
+    var checkOk = "123456789"
+    var checkString = formulario.m.value;
+
+    alert(checkString);
+
+    var allValid = true;
+
+    if (!allValid) {
+        alert("Escriba solo numeros en el campo numerico");
+        formulario.m.focus();
+        return (false);
+    }
+
+    if (checkString);
+
+    //FORMULARIO AÑO
+    if (formulario.a.value.length < 4) {
+        alert("Escriba por lo menos 4 digitos para el año");
+        //focus nos auida a focalizar donde eta el error del llenado
+        //realiza un posicioamiento al elemento del formulario
+        formulario.a.focus();
+        return false;
+    }
+
+    var checkOk = "123456789"
+    var checkString = formulario.a.value;
+
+    alert(checkString);
+
+    var allValid = true;
+
+    if (!allValid) {
+        alert("Escriba solo numeros en el campo numerico");
+        formulario.a.focus();
+        return (false);
+    }
+
+    if (checkString);
 
 
+}
 
+//calculando años bisiesto y reglas para los meses
+
+function fechas() {
+    var dia = getElementByid("d").value;
+    var mes = getElementByid("m").value;
+    var year = getElementByid("a").value;
+
+    if (mes == 01 || mes == 03 || mes == 05 || mes == 07 || mes == 08 || mes == 10 || mes == 12) {
+        dia < 32;
+    } else if (mes == 04 || mes == 06 || mes == 09 || mes == 11) {
+        dia < 31;
+    } else if (mes == 02) {
+        dia < 28;
+    } else if (dia < 1) {
+        alert("dia no existente");
+    } else if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)) {
+        alert("el año" + year + "es bisiesto");
+        if (mes == 2 || mes == 02)
+            dia + 1;
+    } else {
+        alert("el año" + year + "no es bisiesto");
+    }
+}
+
+//id
+function contenido(id) {
+    return document.getElementById(id);
+}
+//porcentaje masculino/femenino
+function calcular() {
+    var h = parseInt(contenido("masculino").value);
+    var m = parseInt(contenido("femenino").value);
+
+    var total = h + m;
+    //var porcentaje_hombre = total / h * 100;
+    //var porcentaje_mujeres = total / m * 100;
+
+    var porcentaje_hombre = h / total * 100;
+    var porcentaje_mujeres = m / total * 100;
+
+    contenido("h_porcentaje").innerHTML = porcentaje_hombre.toFixed(0) + "%";
+    contenido("m_porcentaje").innerHTML = porcentaje_mujeres.toFixed(0) + "%";
+}
+//Calcular edad
+function calcularEdad() {
+    var fecha_Nacimiento = parseInt(contenido("date").value);
+    var fecha_Actual = new Date();
+    var a = fecha_Actual.getFullYear() - fecha_Nacimiento;
+
+    //fecha_Nacimiento.setFullYear(fecha_Actual.getFullYear());
+
+    if (fecha_Actual.getFullYear() < fecha_Nacimiento) {
+        --a;
+        contenido("edad").innerHTML = a;
+        //console.log(a);
+    }
+    contenido("edad").innerHTML = a;
 
 }
